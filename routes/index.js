d436c431ced7195055ec8bee95a26cbac02702bb
Efe Sıradağ <bahtiyarefesiradag@gmail.com>
2024-05-02 02:25:54 +0300

    Rendering items on index router that incoming datas by pageModule

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var pages = require('../modules/pagesModule').pagesModule('views/pages')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  res.render('index', {pages: pages});
 });
 
 module.exports = router;
