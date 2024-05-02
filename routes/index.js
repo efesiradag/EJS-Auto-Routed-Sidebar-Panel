a8b8abe7bc0bc6866918b76ec8046cc6fe63b0de
Efe Sıradağ <bahtiyarefesiradag@gmail.com>
2024-05-02 21:36:36 +0300

    getting route parameter and automatically render the page in pages folder

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var pagesNames = require('../modules/pagesModule').pagesModule('views/pages')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:pageReq', function(req, res, next) {
   
-  res.render('index', {pageName: pagesNames});
+  res.render('index', {pageName: pagesNames, page: req.params.pageReq+'.ejs'});
 });
 
 module.exports = router;
